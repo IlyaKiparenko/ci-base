5a50bf3c1fd6644b87ff893d2816c08b3081b797
IlyaKiparenko <ilya.kiparenko@gmail.com>
2016-05-24 14:40:03 +0300

    Update SepSet.cpp

--- solutions/SeparatedSet/SepSet.cpp
+++ solutions/SeparatedSet/SepSet.cpp
@@ -16,50 +16,56 @@ SepSet::SepSet(int size_) {
 }
 
 void SepSet::add(int i) {
-  if (i >= 0 && i < size)
+  if (i >= 0 && i < size) {
     if (sets[i] == -1) {
       sets[i] = i;
       h[i] = 0;
-    } else
+    } else {
       throw new std::exception("double union");
+    }
   else
     throw new std::exception("wrong index");
+  }
 }
 
 void SepSet::merge(int i, int j) {
-  if (i >= 0 && i < size && j >= 0 && j < size)
+  if (i >= 0 && i < size && j >= 0 && j < size) {
     if (sets[i] != -1 && sets[j] != -1) {
       if (i != j) {
-        while (sets[i] != i) 
+        while (sets[i] != i)
           i = sets[i];
-        while (sets[j] != j) 
+        while (sets[j] != j)
           j = sets[j];
-        if (i != j)
+        if (i != j) {
           if (h[i] < h[j])
             sets[i] = j;
           else
-            if(h[i] > h[j])
+            if (h[i] > h[j]) {
               sets[j] = i;
-            else {
+            } else {
               sets[i] = j;
               h[j] +=1;
             }
+        }
       }
-    } else
+    } else {
       throw new std::exception("double union");
-  else
+    }
+  } else {
     throw new std::exception("wrong index");
+  }
 }
 
 int SepSet::whereI(int i) {
   if (i >= 0 && i < size) {
     if (sets[i] != -1) {
-      while (sets[i] != i) 
+      while (sets[i] != i)
         i = sets[i];
     }
     return sets[i];
-  } else
+  } else {
     throw new std::exception("wrong index");
+  }
 }
 
 bool SepSet::isAdded(int i) {
@@ -68,11 +74,11 @@ bool SepSet::isAdded(int i) {
 
 void SepSet::print() {
   printf("sets = ");
-  for (int i = 0; i < size; i++) 
+  for (int i = 0; i < size; i++)
     printf("%4i ", sets[i]);
   printf("\n");
   printf("h    = ");
-  for (int i = 0; i < size; i++) 
+  for (int i = 0; i < size; i++)
     printf("%4i ", h[i]);
   printf("\n");
 }
